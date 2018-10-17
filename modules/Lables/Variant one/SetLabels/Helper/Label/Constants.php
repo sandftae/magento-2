80597b92798a20cfe64d35155112c2f8338a7d00
sandftae <sandftae@gmail.com>
2018-10-17 21:00:20 +0300

    Update Constants.php

--- modules/Lables/Variant one/SetLabels/Helper/Label/Constants.php
+++ modules/Lables/Variant one/SetLabels/Helper/Label/Constants.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Magecom_SetLabels Constants::class
- *
- * @category Magecom
- * @package Magecom_Workshop
- * @author Magecom
- */
+
 namespace PleaseWork\SetLabels\Helper\Label;
 
 /**
